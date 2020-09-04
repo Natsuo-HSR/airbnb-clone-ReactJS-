4c7db72e4f7ed7b358fb5723635ef21da8687711
Natsuo-pc <natsuo.ht@gmail.com>
2020-09-04 10:18:04 +0300

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage'
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
 
-      {/* Home */}
-        {/* Header */}
+        {/* Render home page or Search page */}
+        <Switch>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
 
-        {/* Banner */}
-          {/* Search */}
-
-        {/* Cards */}
-
-        {/* Footer */}
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
 
 
-      {/* Search page */}
-        {/* Header */}
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -5,9 +5,11 @@ import 'react-date-range/dist/theme/default.css'
 import { DateRangePicker } from 'react-date-range'
 import PersonIcon from '@material-ui/icons/Person'
 import { Button } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
 
 function Search() {
+    const history = useHistory();
     const [startDate, setStartDate] = useState(new Date())
     const [endDate, setEndDate] = useState(new Date())
 
@@ -37,7 +39,7 @@ function Search() {
                 defaultValue={2}
                 type='number'
             />
-            <Button className='search-btn'>Search Airbnb</Button>
+            <Button className='search-btn' onClick={ () => history.push('/search') }>Search Airbnb</Button>
         </div>
     )
 }
